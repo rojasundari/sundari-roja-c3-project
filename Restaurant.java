fddb1cb1ff8d4b9d058cde4b3def616ea7d48bbf
rojasundari <88735608+rojasundari@users.noreply.github.com>
2022-05-22 15:13:32 +0530

    Add files via upload

---
+++ Restaurant.java
@@ -0,0 +1,93 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        if(getCurrentTime().isAfter(this.openingTime) && getCurrentTime().isBefore(this.closingTime))
+        return true;
+        else
+            return false;
+    }
+
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public List<Item> getMenu() {
+        return this.menu;
+
+    }
+
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+
+
+    public int getCost(List<String> items) throws itemNotFoundException {
+        int totalcost = 0;
+        List<String> nonRepeatedItems = removeRepetitions(items);
+        for (String name : nonRepeatedItems) {
+            Item item = findItemByName(name);
+            if (item != null)
+                totalcost += item.getPrice();
+            else
+                throw new itemNotFoundException(name + " Not Found");
+        }
+        return totalcost;
+    }
+
+    /**
+     *
+     * @param items
+     * @return List of non repeated items.
+     */
+    private List<String> removeRepetitions(List<String> items)
+    {
+        Set<String> set = new HashSet<String>(items);
+        return new ArrayList<String>(set);
+    }
+}
